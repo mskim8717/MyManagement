2bf761c1d6a110516915e097a9453123d6874f20
MinsooKim <ms.kim8717@gmail.com>
2020-09-13 21:33:29 +0900

    online study project initial commit!

---
+++ MyManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyManagement
+//
+//  Created by 김민수 on 2020/09/13.
+//  Copyright © 2020 MinsooKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
